86a47f01ac999e2bf86f3c4af78ab195d4b91731
Ishan Agrawal <i1agrawal@ucsd.edu>
2026-02-26 06:57:20 +0800

    Need to figure out why dice loss is so low

--- utility/metrics.py
+++ utility/metrics.py
@@ -4,21 +4,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class DiceCELoss(nn.Module):
-    def __init__(self, weight=torch.tensor([[0.1, 15, 25, 130]]), smooth=1e-5, dice_weight=1.0, ce_weight=1.0):
+    def __init__(self, weight=torch.tensor([[1, 1, 1, 1]]), smooth=1e-5, dice_weight=1.0, ce_weight=1.0):
         super(DiceCELoss, self).__init__()
         self.smooth = smooth
         self.dice_weight = dice_weight
         self.ce_weight = ce_weight
         self.ce = nn.CrossEntropyLoss(weight=weight)
-        # Register as buffer so it moves with .to(device) automatically
+
         if weight is not None:
             self.register_buffer('weight', weight)
         else:
             self.weight = None
 
     def forward(self, logits, targets):
-        target_indices = targets.argmax(dim=1)
-        ce_loss = self.ce(logits, target_indices)
+        #target_indices = targets.argmax(dim=1)
+        #ce_loss = self.ce(logits, target_indices)
 
         probs = F.softmax(logits, dim=1)
 
@@ -34,7 +34,7 @@ class DiceCELoss(nn.Module):
         else:
             dice_loss = dice_loss_per_class.mean()
 
-        loss = self.dice_weight * dice_loss + self.ce_weight * ce_loss
+        loss = self.dice_weight * dice_loss #+ self.ce_weight * ce_loss
 
         return loss
     
